15bd2341b27d5964f91d12b0b2e10e19b32ff4d6
Jared Nichols <jnichols300@gmail.com>
2016-01-15 09:32:56 -0500

    changed to use promises for convention

--- js/script.js
+++ js/script.js
@@ -1,37 +1,52 @@
-// Setting variables
-var webgl, audio, audioO, stats, guiConfig, MeshBasicMaterial, gui;
-var AudioObj, Mesh, WebGl;
+// // Setting variables
+// var webgl, audio, audioO, stats, guiConfig, MeshBasicMaterial, gui;
+// var AudioObj, Mesh, WebGl;
+//
+// audioId = new Audio();
+// audioId.src = 'tunes/BTSM-Religion.mp3';
+// audioId.controls = false;
+// audioId.autoplay = false;
+// audioId.loop = true;
+// document.getElementById('myAudio');
+//
+// // Audio Object
+//
+// AudioObj = function() {
+//
+//         function AudioObj() {
+//
+//           var self = this;
+//
+//             this.audioCtx = new AudioContext();
+//             this.audio = audioId;
+//             this.audioSrc = this.ctx.createMediaElementSource(this.audio);
+//             this.analyser = this.ctx.createAnalyser();
+//             this.audioData = [];
+//
+//             this.audioSrc.connect(this.analyser);
+//             this.audioSrc.connect(this.ctx.destination);
+//             ctx.decodeAudioData(compressedBuffer).then(function(decodedData) {
+//  // use the decoded data here
+// });
+//
+//             this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
+//
+//             this.audio.play();
+//
+//       }
+// };
 
-audioId = new Audio();
-audioId.src = 'tunes/BTSM-Religion.mp3';
-audioId.controls = false;
-audioId.autoplay = false;
-audioId.loop = true;
-document.getElementById('myAudio');
+var context;
+var source, sourceJs;
+var analyser;
+var buffer;
+var url = 'tunes/BTSM-Religion';
+var array = [];
 
-// Audio Object
+var request = new XMLHttpRequest();
+request.open("GET", url, true);
+request.responseType = "arraybuffer";
 
-AudioObj = function() {
-
-        function AudioObj() {
-
-          var self = this;
-
-            this.audioCtx = new AudioContext();
-            this.audio = audioId;
-            this.audioSrc = this.ctx.createMediaElementSource(this.audio);
-            this.analyser = this.ctx.createAnalyser();
-            this.audioData = [];
-
-            this.audioSrc.connect(this.analyser);
-            this.audioSrc.connect(this.ctx.destination);
-
-            this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
-
-            this.audio.play();
-
-      }
-
-
-
+request.onload = function() {
+    // decode audio data
 };
